00b264b4073e4bae6f491662ffc4f43c1af852a1
abdessittir harkaty <abdessattarharkaty@gmail.com>
2021-12-04 10:04:46 +0100

    ok

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { createServer } from "https";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import jwt  from "jsonwebtoken";
 
